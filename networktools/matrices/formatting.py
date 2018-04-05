dcaa641bdfc5c7145604916668c9541ccf159b1c
rahuezo <rahuezo@ucdavis.edu>
2018-04-05 14:09:16 -0700

    Added .gitignore

--- networktools/matrices/formatting.py
+++
@@ -1,4 +0,0 @@
-def add_padding(parts): 
-    max_size = max([len(part) for part in parts])
-
-    return [tuple(part) + ('',)*(max_size - len(part)) for part in parts]
--- networktools/matrices/validation.py
+++
@@ -1,2 +0,0 @@
-def is_pairs_network(rows): 
-    return all([len(row) == 2 for row in rows])
