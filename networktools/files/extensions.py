e3da9540f0c1081a3d17e770008f59cbb7739ed5
rahuezo <rahuezo@ucdavis.edu>
2018-04-05 21:20:55 -0700

    Fixed files.extensions for online with env variable ONLINE

--- networktools/files/extensions.py
+++ networktools/files/extensions.py
@@ -1,10 +1,22 @@
+import os, sys
+
+try: 
+    ONLINE = eval(os.environ['ONLINE'])
+except: 
+    print """You must set ONLINE env variable first.\nIt can either be 0 or 1."""
+    sys.exit()
+
+
 class ExtensionHandler: 
     """
     This class is used to detect the extension of a file.
     """
 
     def __init__(self, input_file): 
-        self.input_file = input_file
+        if not ONLINE: 
+            self.input_file = input_file
+        else: 
+            self.input_file = input_file.name 
 
     def get_extension(self): 
         extension = self.input_file[::-1].split('.')[0][::-1].lower()
